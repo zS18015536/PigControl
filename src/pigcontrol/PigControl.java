5571708de6926186ddad43b1b3a7b8e1656a7a8c
S18015536 <49405375+TonnyHernandez@users.noreply.github.com>
2019-11-06 17:10:55 -0600

    Login y Ventana Menu
    Funcionalidades del Login y Ingreso a la ventana menu

---
+++ src/pigcontrol/PigControl.java
@@ -0,0 +1,17 @@
+
+package pigcontrol;
+
+import java.util.Scanner;
+
+public class PigControl {
+
+    public static void main(String[] args) {
+        
+        Scanner lee = new Scanner(System.in);
+        
+        Login login = new Login();
+        login.setVisible(true);
+                
+    }
+    
+}
